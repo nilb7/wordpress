0f24b175845dac43fc554b9c8a54ea0e5497c54f
nilb7 <tieenn298@gmail.com>
2025-06-10 16:02:55 +0200

    gg

--- functions.php
+++ functions.php
@@ -7,8 +7,40 @@ function load_scripts(){
         array(),
         filemtime(get_template_directory().'/style.css'),'all'
     );
+
+    wp_enqueue_script('dropdown',get_template_directory().'/js/dropdown.js',array(),'1.0',false)
 }
 
 add_action('wp_enqueue_scripts','load_scripts');
+
+function config() {
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu'=>'Main Menu',
+            'wp_devs_footer_menu'=>'Footer Menu'
+        )
+        );
+
+        $args=array(
+            'height'=>225,
+            'width'=>1920
+        );
+
+        add_theme_suppoert('custom-header',$args);
+        add_theme_support('post_thumbnails');
+        add_theme_support('custom-logo',array(
+            'width'=>200,
+            'height'=>110,
+            'flex-height'=>true,
+            'flex-width'=>true
+        ));
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5',array('comment-list','comment-form'.'search-from','gallery','caption','style','script'));
+
+
+    }
+
+
+    add_action('after_setup_theme','config',0);
 
 ?>
