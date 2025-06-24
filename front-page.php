02569b627c0f8bd96929ccdb82ef54c117afe6fb
nilb7 <tieen298@gmail.com>
2025-06-24 06:54:19 -0700

    gg

--- front-page.php
+++ front-page.php
@@ -14,5 +14,79 @@
                 <img src="<?php echo get_template_directory_uri();?>/images/3.jpg ">
             </div>
         </div>
+
+        <div id="overlay">
+            <div id="left_button" class="overlay_button" onclick="onLeftButton()"><</div>
+            <div id="right_button" class="overlay_button" onclick="onRightButton()">></div>
+        </div>
+
     </div>
 </div>
+
+<div style="text-align:center">
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+    <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+
+<div id="content" class="site-content">
+    <div id="primary" class="content-area"></div>
+        <main id="main" class="site-main">
+            <section class="hero">
+                Hero
+            </section>
+            <section class="services">
+                <h2>Services</h2>
+                <div class="container">
+                    <div class="services-item">
+                        <?php
+                            if(is_active_sidebar('services-1')){
+                                dynamic_sidebar('services-1');
+                            }
+                        ?>
+                    </div>
+                    <div class="services-item">
+                        <?php
+                            if(is_active_sidebar('services-2')){
+                                dynamic_sidebar('services-2');
+                            }
+                        ?>
+                    </div>
+                    <div class="services-item">
+                        <?php
+                            if(is_active_sidebar('services-3')){
+                                dynamic_sidebar('services-3');
+                            }
+                        ?>
+                    </div>
+                </div>
+            </section>
+            <section class="home-blog">
+                <h2>Latest News</h2>
+                <div class="container">
+                    <?php
+                    $args=array(
+                        'post_type' =>'post',
+                        'posts_per_page' =>5,
+                        'category__in'=>array(9,10,15),
+                        'category__not_in'=>array(1)
+                    );
+                    
+                    $postlist=new WP_Query($args);
+
+
+                        if($postlist->have_posts()):
+                            while($postlist->have_post()):$postlist->the_posts();
+                            get_template_part('parts/content','latest-news');
+                            endwhile;
+                            wp_reset_postdata();
+                        else:
+                    ?>
+                    <p>Nothing to be displayed!</p>
+                    <?php endif;?>
+
+                </div>
+            </section>
+        </main>
+</div>
+<?php get_footer();?>
